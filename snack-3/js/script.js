31df3b8f78d594d884b9ee8302d4b183e6bc3206
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-28 17:27:48 +0100

    Finished the third snack of the day

---
+++ snack-3/js/script.js
@@ -0,0 +1,29 @@
+const arr1 = [1,2,23,'asdhiuasdh','asdas'];
+const arr2 = [1,2,23,'asdhiuasdh','asdas','asdasdasd',3434];
+
+console.log('Lista 1 prima del lavoro ' + arr1);
+console.log('Lista 2 prima del lavoro ' + arr2);
+
+
+if(arr1.length < arr2.length){
+    alert('La prima lista di array è più piccola della seconda')
+    
+    arrayLengthControl(arr1 , arr2);
+}
+
+else if(arr1.length > arr2.length){
+    alert('La seconda lista di array è più piccola della prima');
+
+    arrayLengthControl(arr2 , arr1);
+}
+
+console.log('Lista 1 dopo il lavoro: ' + arr1);
+console.log('Lista 2 dopo il lavoro: ' + arr2);
+
+
+function arrayLengthControl(a,b){
+    while(a.length < b.length){
+        let newElement = prompt('Inserisci un valore nell\'array');
+        a.push(newElement);
+    }
+}
