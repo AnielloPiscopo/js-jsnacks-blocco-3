9348080fb989705829ed26e0b08a95030740b589
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-28 16:27:01 +0100

    Added the trace

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,3 +1,4 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
 let correctNumber = Math.floor(Math.random() * 10 +1);
 console.log(correctNumber);
 
