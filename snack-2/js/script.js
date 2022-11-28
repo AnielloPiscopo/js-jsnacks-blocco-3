4d930e90d372a6d458311f8f4502a89e253c47ed
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-28 16:44:54 +0100

    Finished the 2 snack

---
+++ snack-2/js/script.js
@@ -0,0 +1,23 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+const numbersList = [];
+
+let sum = 0;
+
+do{
+    userNumber = parseFloat(prompt('Inserisci il tuo numero'));
+    if(Number.isNaN(userNumber)){
+        userNumber = 0;
+    }
+
+    numbersList.push(userNumber);
+    sum+=userNumber;
+
+    if(sum>50){
+        sum-=userNumber;
+        numbersList.pop();
+        break;
+    }
+}while(sum < 50);
+
+console.log(sum);
+console.log(numbersList);
