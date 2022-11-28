eb1f874c01febfe18160ba968c980ab9c3361ff2
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-28 17:39:06 +0100

    Added an improvement

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -9,21 +9,33 @@ if(arr1.length < arr2.length){
     alert('La prima lista di array è più piccola della seconda')
     
     arrayLengthControl(arr1 , arr2);
-}
-
-else if(arr1.length > arr2.length){
+}else if(arr1.length > arr2.length){
     alert('La seconda lista di array è più piccola della prima');
 
     arrayLengthControl(arr2 , arr1);
 }
 
+
 console.log('Lista 1 dopo il lavoro: ' + arr1);
 console.log('Lista 2 dopo il lavoro: ' + arr2);
 
 
+
+
+
+function controlOfValue(a){
+    while(a === null || a === ''){
+        a = prompt('Ridigita il valore');
+    }
+    return a;
+}
+
 function arrayLengthControl(a,b){
     while(a.length < b.length){
         let newElement = prompt('Inserisci un valore nell\'array');
+        
+        newElement = controlOfValue(newElement);
+        
         a.push(newElement);
     }
 }
