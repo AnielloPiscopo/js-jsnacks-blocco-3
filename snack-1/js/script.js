430ce074d1bcbc7319f2551b382e64d45e8b794c
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-28 16:59:53 +0100

    Corrected the first snack

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,11 +1,12 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
-let correctNumber = Math.floor(Math.random() * 10 +1);
-console.log(correctNumber);
-
 let userNumber;
 
-alert('Inserisci un numero da 1 a 10 finché non avrai trovato il valore corretto');
+alert('Inserisci un numero da 1 a 10');
 
 do{
     userNumber = parseFloat(prompt('Digita il tuo numero'));
-}while(userNumber !== correctNumber)
+
+    if(Number.isNaN(userNumber)){
+        userNumber = 0;
+    }
+}while(userNumber>10 || userNumber<1)
--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,10 +1,11 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 const numbersList = [];
-
 let sum = 0;
 
 do{
     userNumber = parseFloat(prompt('Inserisci il tuo numero'));
+
+
     if(Number.isNaN(userNumber)){
         userNumber = 0;
     }
