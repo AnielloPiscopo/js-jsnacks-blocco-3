d7089784e35fe3a8e39ca886458eb5e252904dbb
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-30 11:35:50 +0100

    Added an improvement

--- snack-7/js/script.js
+++ snack-7/js/script.js
@@ -20,7 +20,7 @@
 const divColorRed = document.querySelector('div#color-red');
 const divColorGreen = document.querySelector('div#color-green');
 
-console.log(array)
+console.log(array);
 
 for(let i=0 ; i<array.length ; i++){
     let pElement = getPElements(array[i]);
